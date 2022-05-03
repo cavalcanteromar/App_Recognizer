78b73af51f340e2ecf8ab48031d89c0edf7f7f9a
cavalcanteromar <romariocavalcant@rede.ulbra.br>
2022-05-03 20:31:11 -0300

    first commit

---
+++ App.js
@@ -0,0 +1,25 @@
+import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+
+
+import Home from './views/Home';
+import Result from './views/Result';
+import TFlow from './views/tfw';
+
+const Stack = createNativeStackNavigator();
+
+function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={Home} />
+        <Stack.Screen name="Result" component={Result} />
+        <Stack.Screen name="TFlow" component={TFlow} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default App;
